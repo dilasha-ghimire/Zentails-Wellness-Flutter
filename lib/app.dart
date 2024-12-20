b9107a2217e0943e70a895e3a76cbdefdf6b33fd
dilasha-ghimire <dilashhaa@gmail.com>
2024-12-20 07:14:37 +0545

    update: dashboard and bottom navigation bar created

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:zentails_wellness/view/authentication_screen/login_view.dart';
 import 'package:zentails_wellness/view/authentication_screen/registration_view.dart';
 import 'package:zentails_wellness/view/dashboard_screen/dashboard_view.dart';
 import 'package:zentails_wellness/view/onboarding_screen/onboarding_view.dart';
 import 'package:zentails_wellness/view/splash_screen/splash_screen_view.dart';
-import 'package:zentails_wellness/view/authentication_screen/login_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,12 +16,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Zentails Wellness',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFFCF5D7),
+      ),
       home: const SplashScreen(),
       routes: {
         '/onboarding': (context) => const OnboardingView(),
         '/login': (context) => const LoginPage(),
         '/registration': (context) => const RegistrationView(),
-        '/homepage': (context) => const DashboardView()
+        '/homepage': (context) => const DashboardView(),
       },
     );
   }
