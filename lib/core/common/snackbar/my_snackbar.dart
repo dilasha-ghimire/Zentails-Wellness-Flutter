077af3dc708759fc7faaf9434fdf154e0d748550
dilasha-ghimire <dilashhaa@gmail.com>
2025-03-05 05:07:12 +0545

    update: snackbar updated

--- lib/core/common/snackbar/my_snackbar.dart
+++ lib/core/common/snackbar/my_snackbar.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
-// awesome_snackbar_content: ^0.1.5
-
 showMySnackBar({
   required BuildContext context,
   required String message,
   Color? color,
 }) {
-  ScaffoldMessenger.of(
-    context,
-  ).showSnackBar(
+  final screenWidth = MediaQuery.of(context).size.width;
+
+  bool isTablet = screenWidth >= 600;
+  double fontSize = isTablet ? 24 : 14;
+  double marginHorizontal = isTablet ? screenWidth * 0.2 : screenWidth * 0.1;
+  double paddingVertical = isTablet ? 20 : 16;
+  double borderRadius = isTablet ? 15 : 12;
+
+  ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(message),
-      backgroundColor: color ?? const Color.fromARGB(255, 85, 24, 13),
-      duration: const Duration(seconds: 1),
+      content: Text(
+        message,
+        style: TextStyle(fontSize: fontSize),
+      ),
+      backgroundColor: color ?? Colors.green,
+      duration: const Duration(seconds: 2),
       behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.symmetric(
+        horizontal: marginHorizontal,
+        vertical: paddingVertical,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
     ),
   );
 }
--- lib/features/home/presentation/view/dashboard_screen/dashboard_view.dart
+++ lib/features/home/presentation/view/dashboard_screen/dashboard_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:zentails_wellness/features/home/presentation/view/user_screen/history_view.dart';
 import 'package:zentails_wellness/features/home/presentation/view/user_screen/home_view.dart';
 import 'package:zentails_wellness/features/home/presentation/view/user_screen/profile_view.dart';
+import 'package:zentails_wellness/features/home/presentation/view/user_screen/review_page.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
@@ -13,9 +14,11 @@ class DashboardView extends StatefulWidget {
 class _DashboardViewState extends State<DashboardView> {
   int _selectedIndex = 0;
 
-  final List<Widget> lstBottomScreen = [
+  // Screens for navigation
+  final List<Widget> _screens = [
     const HomeView(),
     const HistoryView(),
+    const ReviewPage(),
     const ProfileView(),
   ];
 
@@ -23,44 +26,57 @@ class _DashboardViewState extends State<DashboardView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        top: true,
-        child: lstBottomScreen[_selectedIndex],
+        child: _screens[_selectedIndex],
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20.0),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: "Home",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.history),
-                label: "History",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: "Profile",
-              ),
-            ],
-            backgroundColor: const Color.fromARGB(222, 93, 64, 55),
-            selectedItemColor: const Color(0xFFFCF5D7),
-            unselectedItemColor: const Color.fromARGB(110, 252, 245, 215),
-            currentIndex: _selectedIndex,
-            onTap: (index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            selectedLabelStyle: const TextStyle(
-                fontSize: 14.0, fontFamily: "Montserrat SemiBold"),
-            unselectedLabelStyle: const TextStyle(
-                fontSize: 12.0, fontFamily: "Montserrat SemiBold"),
-            showSelectedLabels: true,
-            showUnselectedLabels: true,
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFFCF5D7),
+              offset: Offset(0, -1),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: const Color.fromARGB(222, 93, 64, 55),
+              selectedItemColor: const Color(0xFFFCF5D7),
+              unselectedItemColor: const Color.fromARGB(110, 252, 245, 215),
+              currentIndex: _selectedIndex,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.history),
+                  label: "History",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.rate_review),
+                  label: "Reviews",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: "Profile",
+                ),
+              ],
+              selectedLabelStyle: const TextStyle(
+                  fontSize: 14.0, fontFamily: "Montserrat SemiBold"),
+              unselectedLabelStyle: const TextStyle(
+                  fontSize: 12.0, fontFamily: "Montserrat SemiBold"),
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+            ),
           ),
         ),
       ),
